fda25b108ebf39be2819a9e61d0c0c6ca6865a9b
tolangner <tolangner@16dfca22-1d42-11df-8821-87deac751e95>
2010-10-11 21:59:34 +0000

    Commited a first working version (at least on my phone) of KolabDroid within the new Android sync framework. Please test this version on your phones/emulators and give me feedback or change the code accordingly.
    I had to perform the following changes to existing code:
    
    - Because the (SSL-)TrustManagerFactory is now not created within an application any longer but during the sync, it does not have
      access to a context and hence cannot access the internal storage any longer. Hence, I moved the key store to the external storage.
    - Removed the "old" SyncService" from the manifest

---
+++ branches/froyo-sync-framework/src/at/dasz/KolabDroid/Sync/KolabCalendarSyncService.java
@@ -0,0 +1,48 @@
+/*
+ *  Copyright 2010 Tobias Langner <tolangner@gmail.com>; All rights reserved.
+ *
+ *  This file is part of Kolab Sync for Android.
+
+ *  Kolab Sync for Android is free software: you can redistribute it
+ *  and/or modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation, either version 3 of
+ *  the License, or (at your option) any later version.
+
+ *  Kolab Sync for Android is distributed in the hope that it will be
+ *  useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ *  of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  General Public License for more details.
+
+ *  You should have received a copy of the GNU General Public License
+ *  along with Kolab Sync for Android.
+ *  If not, see <http://www.gnu.org/licenses/>.
+ */
+package at.dasz.KolabDroid.Sync;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+/**
+ * Service to handle Account sync. This is invoked with an intent with action
+ * ACTION_AUTHENTICATOR_INTENT. It instantiates the syncadapter and returns its
+ * IBinder.
+ */
+public class KolabCalendarSyncService extends Service {
+	private static final Object sSyncAdapterLock = new Object();
+	private static KolabCalendarSyncAdapter sSyncAdapter = null;
+
+	@Override
+	public void onCreate() {
+		synchronized (sSyncAdapterLock) {
+			if (sSyncAdapter == null) {
+				sSyncAdapter = new KolabCalendarSyncAdapter(getApplicationContext(), true);
+			}
+		}
+	}
+
+	@Override
+	public IBinder onBind(Intent intent) {
+		return sSyncAdapter.getSyncAdapterBinder();
+	}
+}
