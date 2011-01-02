d9437f3fd91ca2c3b54a72de73dd1fe6a2595720
STriller23 <STriller23@16dfca22-1d42-11df-8821-87deac751e95>
2011-01-02 20:04:42 +0000

    finished account manager to convert null accounts (and others) Put into wiki: once converted, go into contacts app, deactivate showing contacts of Kolab account => save and reactivate again to have them shown up

--- branches/froyo-sync-framework/src/at/dasz/KolabDroid/Settings/AccountManager.java
+++ branches/froyo-sync-framework/src/at/dasz/KolabDroid/Settings/AccountManager.java
@@ -1,8 +1,16 @@
 package at.dasz.KolabDroid.Settings;
 
+//We probably wont need this account manager
+//once one installs the branch and adds the sync account, all contact with "null" accounts are changed
+//to be KolabAndroidAccounts
+
 import android.accounts.Account;
 import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
@@ -14,12 +22,22 @@ import android.widget.CompoundButton;
 import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.ListView;
 import android.widget.Spinner;
+import android.widget.Toast;
 import at.dasz.KolabDroid.R;
+import at.dasz.KolabDroid.ContactsContract.Contact;
+import at.dasz.KolabDroid.ContactsContract.ContactDBHelper;
+import at.dasz.KolabDroid.Sync.SyncException;
 
 public class AccountManager extends Activity
 {
+	public static final String ACC_DELIMITER = "--";
+	
 	private ListView contactList = null;
-	//private CheckBox cBoxAll
+	private String destAccountName = "";
+	private String destAccountType = "";
+	
+	private String srcAccountName = "";
+	private String srcAccountType = "";
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
@@ -28,7 +46,7 @@ public class AccountManager extends Activity
 				
 		setContentView(R.layout.account_manager);
 	
-		Log.i("AccM:", "I am the AccountManager");
+		//Log.i("AccM:", "I am the AccountManager");
 		
 		Button doit = (Button) findViewById(R.id.AccmanagerButtonDoit);
 		ButtonPressedListener bpl = new ButtonPressedListener(this);
@@ -37,12 +55,14 @@ public class AccountManager extends Activity
 		Account[] accounts = android.accounts.AccountManager.get(this).getAccounts();
 		Log.i("AccM", "Amount of accounts: " + accounts.length);
 			
-		String[] oldAccItems = new String[accounts.length +1];
-		oldAccItems[0] = "null";
-		int i = 1;
+		String[] oldAccItems = new String[accounts.length + 1];
+		//String[] oldAccItems = new String[accounts.length +2]; //for dummy add 2 :)
+		oldAccItems[0] = "null" + ACC_DELIMITER +"null";
+		//oldAccItems[1] = "Dummy"+ACC_DELIMITER+"DummyType"; //to play around with
+		int i = 1; //int i = 2;
 		for(Account acc: accounts)
 		{
-			oldAccItems[i] = acc.name;
+			oldAccItems[i] = acc.name + ACC_DELIMITER + acc.type;
 			i++;
 		}
 		
@@ -51,15 +71,15 @@ public class AccountManager extends Activity
 		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 		accSpinner.setAdapter(adapter);
 		
-		OnItemSelectedListener spinnerListener = new AccountSelectedListener(this);
+		OnItemSelectedListener spinnerListener = new AccountSelectedListener(this, "source");
 		accSpinner.setOnItemSelectedListener(spinnerListener);
 		
-		
-		String[] newAccItems = new String[accounts.length];
-		i = 0;
+		String[] newAccItems = new String[accounts.length+1];
+		newAccItems[0] = "Dummy"+ACC_DELIMITER+"DummyType";
+		i = 1;
 		for(Account acc: accounts)
 		{
-			newAccItems[i] = acc.name;
+			newAccItems[i] = acc.name + ACC_DELIMITER + acc.type;
 			i++;
 		}
 		
@@ -68,16 +88,12 @@ public class AccountManager extends Activity
 		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 		newAccSpinner.setAdapter(newAccAdapter);
 		
-		
-		//TODO: make this a list of raw contacts from contacts2.db
-		
-		String lv_items[] = { "Android", "iPhone", "BlackBerry" };
-
-		
+		OnItemSelectedListener newSpinnerListener = new AccountSelectedListener(this, "dest");
+		newAccSpinner.setOnItemSelectedListener(newSpinnerListener);
+	
 		contactList = (ListView) findViewById(R.id.AccManagerContactList);
 		
 		//Set option as Multiple Choice. So that user can able to select more the one option from list
-		contactList.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_multiple_choice, lv_items));
 		contactList.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
 		
 		//uncheck all
@@ -91,19 +107,15 @@ public class AccountManager extends Activity
 		cBoxAll.setOnCheckedChangeListener(new OnCheckedChangeListener() {
 			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked)
 			{
-				if (buttonView.isChecked()) { 
-//					Toast.makeText(getBaseContext(), "Checked", 
-//					Toast.LENGTH_SHORT).show();
-					
+				if (buttonView.isChecked())
+				{
 					for(int i=0; i<contactList.getCount(); i++)
 					{
 						contactList.setItemChecked(i, true);
 					}
 				} 
 				else 
-				{ 
-//					Toast.makeText(getBaseContext(), "UnChecked", 
-//					Toast.LENGTH_SHORT).show();
+				{
 					for(int i=0; i<contactList.getCount(); i++)
 					{
 						contactList.setItemChecked(i, false);
@@ -112,24 +124,89 @@ public class AccountManager extends Activity
 			} 
 		});
 		
+		//Log.i("AccM:", "created");		
+	}
+	
+	public String getDestAccountName()
+	{
+		return destAccountName;
+	}
+
+	public String getDestAccountType()
+	{
+		return destAccountType;
+	}
+	
+	public void updateContactList()
+	{
+		Log.i("AccM:", "Will filter list with: " + srcAccountName + " of type " + srcAccountType);
 		
+		ContentResolver cr = getContentResolver();
+			
+		String where = ContactsContract.RawContacts.ACCOUNT_NAME + "=?";
+			
+		Cursor dbContacts = null;
+		
+		if("null".equals(srcAccountName))
+		{
+			dbContacts = cr.query(ContactsContract.RawContacts.CONTENT_URI, null, where,
+				new String[]{""}, null);
+		}
+		else
+		{				
+			dbContacts = cr.query(ContactsContract.RawContacts.CONTENT_URI, null, where,
+					new String[]{srcAccountName}, null);
+		}
+		
+		int count = dbContacts.getCount();
+		
+		Contact[] cs = new Contact[count];
+		
+		if(dbContacts != null && dbContacts.moveToFirst())
+		{
+			int rawIDCol = dbContacts.getColumnIndex(ContactsContract.RawContacts._ID);
+			
+			int i=0;
+			do
+			{
+				int id = dbContacts.getInt(rawIDCol);
+				try
+				{
+					cs[i] = ContactDBHelper.getContactByRawID(id, cr);
+				}
+				catch (SyncException ex)
+				{
+					// TODO Auto-generated catch block
+					ex.printStackTrace();
+				}
+				
+				i++;
+			}
+			while(dbContacts.moveToNext());
+		}		
+		contactList.setAdapter(new ArrayAdapter<Contact>(this, android.R.layout.simple_list_item_multiple_choice, cs));
+		
+		return;
 		
 	}
 	
-	public void updateContactList(String filter)
+	public void setDestAccount(String acc)
 	{
-		Log.i("AccM:", "Will filter list with: " + filter);
-		
-		if(filter == null) return;
-		
-		if("null".equals(filter))
+		String[] tokens = acc.split(AccountManager.ACC_DELIMITER);
+		if(tokens.length == 2)
 		{
-			return; //for testing => later we have contacts with no account here
+			this.destAccountName = tokens[0];
+			this.destAccountType = tokens[1];
 		}
-		else
+	}
+	
+	public void setSrcAccount(String acc)
+	{
+		String[] tokens = acc.split(AccountManager.ACC_DELIMITER);
+		if(tokens.length == 2)
 		{
-			String lv_items[] = {"eins", "zwei", "drei"};
-			contactList.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_multiple_choice, lv_items));
+			this.srcAccountName = tokens[0];
+			this.srcAccountType = tokens[1];
 		}
 	}
 	
@@ -141,6 +218,7 @@ public class AccountManager extends Activity
 	public class ButtonPressedListener implements View.OnClickListener
 	{
 		private AccountManager accM = null;
+		private ContentResolver cr = getContentResolver();
 		
 		public ButtonPressedListener(AccountManager accM)
 		{
@@ -149,19 +227,38 @@ public class AccountManager extends Activity
 
 		public void onClick(View v)
 		{
-			// TODO Auto-generated method stub
 			Log.i("AccM:", "Button do it pressed");
 			ListView contactList = accM.getContactList();
+			
+			String destAccName = accM.getDestAccountName();
+			String destAccType = accM.getDestAccountType();
+			
+			int checkedContacts = 0;
+			int updatedContacts = 0;
 			
 			for(int i=0; i<contactList.getCount(); i++)
 			{
 				if(contactList.isItemChecked(i))
 				{
-					String contactChecked = (String) contactList.getItemAtPosition(i);
-					Log.i("AccM:", "Need to convert entry: " + contactChecked);
+					checkedContacts++;
+					
+					Contact contactChecked = (Contact) contactList.getItemAtPosition(i);
+					Log.i("AccM:", "Need to convert entry: " + contactChecked + " to Account: " + destAccName + "of type: " + destAccType);
+					
+					String selection = ContactsContract.RawContacts._ID + "=?";										
+					
+					ContentValues cvs = new ContentValues();
+					cvs.put(ContactsContract.RawContacts.ACCOUNT_NAME, destAccName);
+					cvs.put(ContactsContract.RawContacts.ACCOUNT_TYPE, destAccType);
+					
+					int rowsUpdated = cr.update(ContactsContract.RawContacts.CONTENT_URI, cvs, selection, new String[] {String.valueOf(contactChecked.getId())});
+					if(rowsUpdated == 1)
+						updatedContacts++;					
 				}
 			}
 			
+			this.accM.updateContactList();
+			Toast.makeText(getBaseContext(), updatedContacts + " of "+ checkedContacts+" converted", Toast.LENGTH_LONG).show();
 		}
 		
 	}
@@ -169,32 +266,36 @@ public class AccountManager extends Activity
 	public class AccountSelectedListener implements OnItemSelectedListener
 	{
 		private AccountManager accM = null;
+		private String sourceDestType = "source";
 		
-		public AccountSelectedListener(AccountManager accM)
+		public AccountSelectedListener(AccountManager accM, String type)
 		{
 			this.accM = accM;
+			this.sourceDestType = type;
 		}
 		
-		public void onItemSelected(AdapterView<?> element, View arg1, int pos,
-				long arg3)
+		public void onItemSelected(AdapterView<?> element, View arg1, int pos, long arg3)
 		{
-			Log.i("AccM:", "on selected with item at pos: " + pos);
+			Log.i("AccM:", "on selected SOURCE with item at pos: " + pos);
 			if(element instanceof Spinner)
 			{
 				Spinner accSpin = (Spinner) element;
 				String str = (String) accSpin.getItemAtPosition(pos);
 				//Log.i("AccM:", "spinner");
-				this.accM.updateContactList(str);
-				
+				if("source".equals(sourceDestType))
+				{
+					this.accM.setSrcAccount(str);
+					this.accM.updateContactList();
+				}
+				else
+				{
+					this.accM.setDestAccount(str);
+				}
 			}
-			
 		}
 
 		public void onNothingSelected(AdapterView<?> arg0)
-		{
-			// TODO Auto-generated method stub
-			
-		}
+		{}
 		
 	}
 	
